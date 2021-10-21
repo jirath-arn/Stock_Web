cc601eeadcf348455ac584f2d3e8a4c8e0e91788
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-10-21 22:30:11 +0700

    Home page
    add image to display home page
    and display image to home page 20 %

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+        <div class="col-md-11">
+            <div class="container-wrap">
+                <div class="row no-gutters d-flex mt-5">
+             
+                     {{-- @foreach ($data[0] as $item)  --}}
+                        {{-- @if ($item->status_approve == true) --}}
+                            <div class = "card col-3 d-flex ftco-animate fadeInUp ftco-animated  " style="width:300px; ">
+                                <img src="/img/test-shirt/1.png"  alt="Card image" height="300px">
+                                <p style="font-size: 18px;font-family: Sans-serif;font-style: normal;font-weight: bold;">รหัส 001</p>
+                                <p style="font-size: 14px;font-style: normal;">จำนวนคงเหลือ {{50}} ตัว</p>
+                            </div>
+                        {{-- @endif --}}
+                    {{-- @endforeach --}}
                 </div>
             </div>
         </div>
