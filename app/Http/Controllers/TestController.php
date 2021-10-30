6b167c1d9fa0dc5f9be438c15c7c651bfce4af4e
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-10-30 16:29:20 +0700

    Setting web.php
    setting web.php and TestController for testing page

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('admin.newStock');
+    }
+    public function show()
+    {
+        return view('detail');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CRUDs\RoleController;
 use App\Http\Controllers\CRUDs\UserController;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestController;
 
 // -----------------------------------------------------------------------------------
 
@@ -15,6 +16,8 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/newstock', [TestController::class, 'index'])->name('newstock');
+Route::get('/detail', [TestController::class, 'show'])->name('detail');
 
 Auth::routes();
 
