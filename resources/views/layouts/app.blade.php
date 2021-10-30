8600f0c2f016c7e8bea76b0c2d22b48c3519b94d
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-10-30 16:30:54 +0700

    fix page เพิ่มรายการสินค้าใหม่
    create view page of newStock.blade.php and coding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
             margin-right: auto;
 
         }
+        
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -80,7 +81,7 @@
 
                         @can('product_add')
                         <li class="nav-item">
-                            <a class="nav-link" href="/home">{{ __('เพิ่มสินค้า') }}</a>
+                            <a class="nav-link" href="/newstock">{{ __('เพิ่มสินค้า') }}</a>
                         </li>
                         @endcan
 
