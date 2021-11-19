ac9f9b2091f08448cb65eccd57fbf1b2747bf26f
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-11-19 17:12:53 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+use App\Models\ProductDetail;
+use App\Models\Category;
+use App\Models\Image;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +28,31 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $product = Product::all();
+        $category = Category::all();
+        $image = Image::all();
+        $array_image = array();
+        $product_detail = ProductDetail::all();
+        $array_number = array();
+        $balance = 0;
+        foreach($product as $p){
+            foreach($product_detail as $d){
+                if($p->code_name == $d->product_code_name){
+                   $balance += $d->balance_amount;
+                }
+            }
+            array_push($array_number,$balance);
+            $balance = 0 ;
+
+            foreach($image as $i){
+                if($i->product_code_name == $p->code_name){
+                    array_push($array_image,$i->filename);
+                 }
+            }
+           
+        }
+        
+        $data = [$product,$array_number,$category,$array_image];
+        return view('home', compact('data'));
     }
 }
