20331e30e5475e734a22f40bd2532f13f2318971
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-11-25 18:50:05 +0700

    Update ProductController.php

--- app/Http/Controllers/CRUDs/ProductController.php
+++ app/Http/Controllers/CRUDs/ProductController.php
@@ -3,33 +3,108 @@
 namespace App\Http\Controllers\CRUDs;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 use Symfony\Component\HttpFoundation\Response;
 use Gate;
 use App\Models\Product;
 use App\Models\ProductDetail;
+use App\Models\Category;
+use PhpParser\Node\Stmt\Foreach_;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        // abort_if(Gate::denies('product_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        $product = Product::all();
+        $category = Category::all();
+        $image = Image::all();
+        $array_image = array();
+        $product_detail = ProductDetail::all();
+        $array_number = array();
+        $balance = 0;
+        foreach($product as $p){
+            foreach($product_detail as $d){
+                if($p->code_name == $d->product_code_name){
+                   $balance += $d->balance_amount;
+                }
+            }
+            array_push($array_number,$balance);
+            $balance = 0 ;
+
+            foreach($image as $i){
+                if($i->product_code_name == $p->code_name){
+                    array_push($array_image,$i->filename);
+                 }
+            }
+           
+        }
+        
+        $data = [$product,$array_number,$category,$array_image];
+        return view('home', compact('data'));
     }
 
-    public function show(Product $product)
+    public function show($product)
     {
-        // abort_if(Gate::denies('product_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        $product_detail = ProductDetail::all()->where('product_code_name',$product);
+        $total_amount = 0;
+        $balance_amount = 0;
+        $Image = Image::all()->where('product_code_name',$product);
+        foreach($product_detail as $item){
+            $total_amount +=  $item->total_amount;
+        }
+        
+        foreach($product_detail as $item){
+            $balance_amount +=  $item->balance_amount;
+        }
+
+        $data = [$product_detail,$total_amount,$balance_amount,$Image];
+        return view('detail',compact('data'));
     }
     
     public function create()
     {
-        // abort_if(Gate::denies('product_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        $data = Category::all();
+        return view('admin.newStock', compact('data'));
     }
 
     public function store(Request $request)
-    {
-        // Code..
+    {   
+        $data=$request->all();
+        
+        $request->validate([
+            'product_name' => 'required',
+            'code_name' => 'required',
+            'category' => 'required',
+            'price' => 'required',
+            'color.*' => '',
+            'size' => '',
+            'quantity' => '',
+        ]);
+        
+        
+
+        
+        $product = new Product();
+        $product->code_name = $request->code_name;
+        $product->product_name = $request->product_name;
+        $product->category_id = $request->category;
+        $product->wholesale_price = $request->price;
+        $product->save();
+
+        for($i = 0; $i < count($request->color); $i++){
+            $product_detail = new ProductDetail();
+            $product_detail->product_code_name = $request->code_name;
+            $product_detail->product_color = $request->color[$i];
+            $product_detail->product_size = $request->size[$i];
+            $product_detail->balance_amount = $request->quantity[$i];
+            $product_detail->total_amount = $request->quantity[$i];
+            $product_detail->save();
+        }
+
+        
+        return redirect()->route('products.index');
     }
 
     public function edit(Product $product)
