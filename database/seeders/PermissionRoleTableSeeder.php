42f1c440bc93f74c376d67ae47fc31c1fa66e9dd
Jirath Arnamnath <ice191913@gmail.com>
2021-11-28 07:38:04 +0700

    Updated Permission and Role Gate

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -12,5 +12,10 @@ class PermissionRoleTableSeeder extends Seeder
     {
         $admin_permissions = Permission::all();
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
+
+        $user_permissions = $admin_permissions->filter(function ($permission) {
+            return substr($permission->title, 0, 8) == 'product_';
+        });
+        Role::findOrFail(2)->permissions()->sync($user_permissions);
     }
 }
