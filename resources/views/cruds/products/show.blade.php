24cf275975c8d729ac13d42e736fa6ebcbf4fab4
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-11-29 00:37:27 +0700

    Update show.blade.php
    เพิ่ม ปุ่ม ลบ Product Detail และ  ลบ Product

--- resources/views/cruds/products/show.blade.php
+++ resources/views/cruds/products/show.blade.php
@@ -120,8 +120,16 @@
         <div class="col-xl-6 col-lg-6 col-sm-12">
             <h3>{{ __('รหัส') }} {{ $product->code_name }} ({{ $product->product_name }})</h3>
             <label>{{ __('จำนวนคงเหลือทั้งหมด') }} {{ $product_balance }} {{ __('ตัว') }}</label>
-            
-            <table class="table table-success">
+            <a href="#" class="btn btn-outline-danger float-right">
+                ลบรายการนี้ทั้งหมด
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash "  viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+            </a>
+
+           
+            <table class="table table-success mt-3">
                 <thead>
                     <tr>
                         <th scope="col">{{ __('สีของสินค้า') }}</th>
@@ -136,6 +144,15 @@
                             <td>{{ $detail->product_color }}</td>
                             <td>{{ $detail->product_size }}</td>
                             <td>{{ $detail->balance_amount }}</td>
+                            <td> 
+                                
+                                <a href="#"class="btn btn-sm btn-danger" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash "  viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                    </svg>
+                                </a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
